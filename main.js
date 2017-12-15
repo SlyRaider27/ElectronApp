6983fbe7adf74f906ad44d2d9933440ef0590762
Megan Britt <mb0783333@otc.edu>
2017-12-14 20:39:19 -0600

    Push

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+const ipc = electron.ipcMain;
+var jreq = require('json-file');
+var filesys = require('fs');
+const Menu = electron.Menu;
 
-app.on('ready', function(){
+app.on('ready', function () {
     mainWindow = new BrowserWindow();
-    width:300;
-    height:200;
+    width: 300;
+    height: 200;
     mainWindow.loadURL(`file://${__dirname}/index.html`)
-    mainWindow.on('closed', function(){
-           mainWindow = null
-       })
-})
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    });
+});
+
+ipc.on('open', (event, filePath) => {
+    var readFile = jreq.read(filePath);
+    var jObject = readFile.get("items");
+    event.sender.send("openRen", jObject);
+});
+
+const menuTemp = [{
+    label: "Open File",
+    click: () => { mainWindow.webContents.send('modal-open') }
+},
+{
+    label: "Dev Tool",
+    click: () => {mainWindow.toggleDevTools() }
+},
+{   label: "Add Task",
+    click: () => {mainWindow.webContents.send('modal-addTask')}
+},
+{
+    label: "Delete Items",
+    click: () => {mainWindow.webContents.send('modal-deleteAll')}
+},
+{
+    label: "Close",
+    click: () => {mainWindow.close()}
+}];
+const menu = Menu.buildFromTemplate(menuTemp);
+Menu.setApplicationMenu(menu);
--- script.js
+++ script.js
@@ -1,25 +1,45 @@
+const { ipcRenderer } = require('electron');
 var app = new Vue({
-      el: "#app",
-      data: {
-        tasks: {
-          items: []
-        }
-      },
-      methods: {
-        addTask(){
-            var task = document.getElementById("taskPrompt").value;
-            var len = this.tasks.items.length;
-            this.tasks.items.push({taskName:task, completed:false, id:len});
-        },
-        completeThis(id) {
-         this.tasks.items[id].completed = true;
-        },
-        deleteAll() {
-          this.tasks.items = [];
-        }
-      }
-    });
-
-    document.getElementById("openFileButton").addEventListener('click', _=>{
-        console.log(document.getElementById("openFileName").files[0].path)
-    });
+  el: "#app",
+  data: {
+    tasks: {
+      items: []
+    }
+  },
+  methods: {
+    addTask() {
+      var task = document.getElementById("taskPrompt").value;
+      var len = this.tasks.items.length;
+      this.tasks.items.push({ taskName: task, completed: false, id: len });
+    },
+    completeThis(id) {
+      this.tasks.items[id].completed = true;
+    },
+    deleteAll() {
+      this.tasks.items = [];
+    }
+  }
+});
+//open file ipc
+document.getElementById("openFileButton").addEventListener('click', _ => {
+  var path = (document.getElementById("openFileName").files[0].path);
+  ipcRenderer.send("open", path);
+});
+ipcRenderer.on('openRen', (event, items) => {
+  app.tasks.items = items;
+});
+// Here is the logic for the menu. Again, 
+//JQuery was being a butt so I tried my best and this logic by all means should work.
+//I have added console logs so you can see they should be working.
+ipcRenderer.on('modal-open', (event) => {
+  console.log("Modal open! I WORK!");
+  $("#modal3").modal("show");
+});
+ipcRenderer.on('modal-addTask', (event) => {
+  console.log("Modal Add Task! I WORK!")
+  $("#modal1").modal("show");
+});
+ipcRenderer.on('modal-deleteAll', (event) => {
+  console.log("Model delete! I WORK!")
+   $("#modal2").modal("show");
+});
